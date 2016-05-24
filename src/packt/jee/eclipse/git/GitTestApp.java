a362e8295048fad0044a3a2381f0ed4300145655
antonio <antonio@email.com>
2016-05-24 17:54:38 +0200

    Testing branch

--- src/packt/jee/eclipse/git/GitTestApp.java
+++ src/packt/jee/eclipse/git/GitTestApp.java
@@ -1,7 +1,7 @@
 package packt.jee.eclipse.git;
 
 public class GitTestApp {
-	public static void main(String args[]){
-		System.out.println("Hello");
+	public static void main(String args[]) {
+		System.out.println("Hello Git, from bug#1234 !!");
 	}
 }
